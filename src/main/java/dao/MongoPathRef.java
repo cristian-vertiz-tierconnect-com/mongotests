a5e55516c7cf22c48124c8eb87b01f95c4b70cfc
Ruth Chirinos <ruth.chirinos@mojix.com>
2016-03-29 16:06:25 -0400

    Option: Materialized paths

---
+++ src/main/java/dao/MongoPathRef.java
@@ -0,0 +1,108 @@
+package dao;
+
+import com.mongodb.*;
+import org.jose4j.json.internal.json_simple.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+
+/**
+ * Created by rchirinos on 3/29/16.
+ */
+public class MongoPathRef {
+
+
+    static public void main(String[] args) throws IOException {
+        Mongo mongo = new Mongo("localhost", 27017);
+        DB db = mongo.getDB("riot_main");
+
+        pathMongo(db);
+    }
+
+    //Materialized paths
+    public static void ancestorsMongo(DB db) {
+        BasicDBObject query = new BasicDBObject("path", "\\/^,1,3,5,7,\\/");
+
+        DBCursor cursorpath = db.getCollection("path_things").find(query);
+
+        // parse results
+        while (cursorpath.hasNext()) {
+            DBObject record = cursorpath.next();
+            System.out.println(record);
+        }
+    }
+
+    //Ancestors paths
+    public static void pathMongo(DB db) {
+        //get father
+        BasicDBObject query = new BasicDBObject("serialNumber", "BOX1000");
+        DBObject pivote = db.getCollection("path_things").findOne(query);
+        String pathPivote = pivote.get("path").toString();
+        Map<String,Object> dataFinal = null;
+
+        dataFinal = getParents(db, pathPivote,pivote);
+
+        BasicDBObject query2 = new BasicDBObject("path", Pattern.compile(pathPivote));
+        DBCursor pivoteData2 = db.getCollection("path_things").find(query2);
+        List<Object> data = new ArrayList<>();
+
+        while( pivoteData2.hasNext() )
+        {
+            DBObject record = pivoteData2.next();
+            data.add(record);
+        }
+
+       // dataFinal = getChildren(dataFinal,data, 0);
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.putAll( dataFinal );
+        System.out.printf( "JSON: %s", jsonObject );
+
+    }
+
+    //Children
+    public static Map<String,Object> getChildren(Map<String,Object> dataFinal, List<Object> data, int a)
+    {
+        Map<String, Object> dataMap = new HashMap<>();
+        if(a <= data.size())
+        {
+            a = a+1;
+            dataFinal.put("children",getChildren((Map)data.get(a),data,a));
+        }
+
+        return dataMap;
+    }
+
+    //Parents
+    public static Map<String,Object> getParents(DB db, String path, DBObject pivote)
+    {
+        Map<String, Object> dataMap = new HashMap<>();
+        String newPath = "";
+        String[] data = path.split(",");
+        BasicDBObject query = new BasicDBObject("_id", Integer.parseInt(data[0]));
+        DBObject dbObject= db.getCollection("path_things").findOne(query);
+        dataMap.putAll((Map) dbObject);
+
+        for(int i=1;i<data.length;i++)
+        {
+            newPath = newPath+data[i]+",";
+        }
+
+        if(newPath!=null && !newPath.trim().isEmpty())
+        {
+            newPath = newPath.substring(0,newPath.length()-1);
+            dataMap.put("children",getParents(db,newPath,pivote));
+        }else
+        {
+            dataMap.put("children",pivote);
+        }
+        return dataMap;
+    }
+
+
+}
---
+++ src/main/resources/InsertMongoPath.js
@@ -0,0 +1,256 @@
+db.path_things.insert({
+    "_id" : NumberLong(1),
+    "groupTypeId" : NumberLong(3),
+    "groupTypeName" : "Store",
+    "groupTypeCode" : "",
+    "groupId" : NumberLong(3),
+    "groupCode" : "SM",
+    "groupName" : "Santa Monica",
+    "thingTypeId" : NumberLong(1),
+    "thingTypeCode" : "pallete_code",
+    "thingTypeName" : "Pallete",
+    "name" : "P10000",
+    "serialNumber" : "P10000",
+    "eNode" : {
+        "thingTypeFieldId" : NumberLong(5),
+        "time" : ISODate("2016-03-24T16:09:15.346Z"),
+        "value" : "x3ed9371"
+    },
+    "status" : {
+        "thingTypeFieldId" : NumberLong(12),
+        "time" : ISODate("2016-03-24T16:19:03.728Z"),
+        "value" : "closed"
+    },
+    "path" : null
+});
+
+/* 2 */
+db.path_things.insert({
+    "_id" : NumberLong(2),
+    "groupTypeId" : NumberLong(3),
+    "groupTypeName" : "Store",
+    "groupTypeCode" : "",
+    "groupId" : NumberLong(3),
+    "groupCode" : "SM",
+    "groupName" : "Santa Monica",
+    "thingTypeId" : NumberLong(1),
+    "thingTypeCode" : "default_rfid_thingtype",
+    "thingTypeName" : "Default RFID Thing Type",
+    "name" : "RFID1000",
+    "serialNumber" : "RFID1000",
+    "status" : {
+        "thingTypeFieldId" : NumberLong(12),
+        "time" : ISODate("2016-03-24T16:19:03.728Z"),
+        "value" : "active"
+    },
+    "path" : "1"
+});
+
+/* 3 */
+db.path_things.insert({
+    "_id" : NumberLong(3),
+    "groupTypeId" : NumberLong(3),
+    "groupTypeName" : "Store",
+    "groupTypeCode" : "",
+    "groupId" : NumberLong(3),
+    "groupCode" : "SM",
+    "groupName" : "Santa Monica",
+    "thingTypeId" : NumberLong(1),
+    "thingTypeCode" : "cartoon_code",
+    "thingTypeName" : "Cartoon",
+    "name" : "CARTOON1000",
+    "serialNumber" : "CARTOON1000",
+    "status" : {
+        "thingTypeFieldId" : NumberLong(12),
+        "time" : ISODate("2016-03-24T16:19:03.728Z"),
+        "value" : "active"
+    },
+    "path" : "1"
+});
+
+/* 4 */
+db.path_things.insert({
+    "_id" : NumberLong(4),
+    "groupTypeId" : NumberLong(3),
+    "groupTypeName" : "Store",
+    "groupTypeCode" : "",
+    "groupId" : NumberLong(3),
+    "groupCode" : "SM",
+    "groupName" : "Santa Monica",
+    "thingTypeId" : NumberLong(1),
+    "thingTypeCode" : "default_rfid_thingtype",
+    "thingTypeName" : "Default RFID Thing Type",
+    "name" : "RFID1001",
+    "serialNumber" : "RFID1001",
+    "status" : {
+        "thingTypeFieldId" : NumberLong(12),
+        "time" : ISODate("2016-03-24T16:19:03.728Z"),
+        "value" : "active"
+    },
+    "path" : "1,3"
+});
+
+/* 5 */
+db.path_things.insert({
+    "_id" : NumberLong(5),
+    "groupTypeId" : NumberLong(3),
+    "groupTypeName" : "Store",
+    "groupTypeCode" : "",
+    "groupId" : NumberLong(3),
+    "groupCode" : "SM",
+    "groupName" : "Santa Monica",
+    "thingTypeId" : NumberLong(1),
+    "thingTypeCode" : "cartoon_code",
+    "thingTypeName" : "Cartoon",
+    "name" : "BOX1000",
+    "serialNumber" : "BOX1000",
+    "status" : {
+        "thingTypeFieldId" : NumberLong(12),
+        "time" : ISODate("2016-03-24T16:19:03.728Z"),
+        "value" : "active"
+    },
+    "path" : "1,3"
+});
+
+/* 6 */
+db.path_things.insert({
+    "_id" : NumberLong(6),
+    "groupTypeId" : NumberLong(3),
+    "groupTypeName" : "Store",
+    "groupTypeCode" : "",
+    "groupId" : NumberLong(3),
+    "groupCode" : "SM",
+    "groupName" : "Santa Monica",
+    "thingTypeId" : NumberLong(1),
+    "thingTypeCode" : "default_rfid_thingtype",
+    "thingTypeName" : "Default RFID Thing Type",
+    "name" : "RFID1002",
+    "serialNumber" : "RFID1002",
+    "status" : {
+        "thingTypeFieldId" : NumberLong(12),
+        "time" : ISODate("2016-03-24T16:19:03.728Z"),
+        "value" : "active"
+    },
+    "path" : "1,3,5"
+});
+
+/* 7 */
+db.path_things.insert({
+    "_id" : NumberLong(7),
+    "groupTypeId" : NumberLong(3),
+    "groupTypeName" : "Store",
+    "groupTypeCode" : "",
+    "groupId" : NumberLong(3),
+    "groupCode" : "SM",
+    "groupName" : "Santa Monica",
+    "thingTypeId" : NumberLong(1),
+    "thingTypeCode" : "cartoon_code",
+    "thingTypeName" : "Cartoon",
+    "name" : "ITEM1000",
+    "serialNumber" : "ITEM1000",
+    "status" : {
+        "thingTypeFieldId" : NumberLong(12),
+        "time" : ISODate("2016-03-24T16:19:03.728Z"),
+        "value" : "active"
+    },
+    "path" : "1,3,5"
+});
+
+/* 8 */
+db.path_things.insert({
+    "_id" : NumberLong(8),
+    "groupTypeId" : NumberLong(3),
+    "groupTypeName" : "Store",
+    "groupTypeCode" : "",
+    "groupId" : NumberLong(3),
+    "groupCode" : "SM",
+    "groupName" : "Santa Monica",
+    "thingTypeId" : NumberLong(1),
+    "thingTypeCode" : "default_rfid_thingtype",
+    "thingTypeName" : "Default RFID Thing Type",
+    "name" : "RFID1003",
+    "serialNumber" : "RFID1003",
+    "status" : {
+        "thingTypeFieldId" : NumberLong(12),
+        "time" : ISODate("2016-03-24T16:19:03.728Z"),
+        "value" : "active"
+    },
+    "path" : "1,3,5,7"
+});
+
+/* 9 */
+db.path_things.insert({
+    "_id" : NumberLong(9),
+    "groupTypeId" : NumberLong(3),
+    "groupTypeName" : "Store",
+    "groupTypeCode" : "",
+    "groupId" : NumberLong(3),
+    "groupCode" : "SM",
+    "groupName" : "Santa Monica",
+    "thingTypeId" : NumberLong(1),
+    "thingTypeCode" : "pallete_code",
+    "thingTypeName" : "Pallete",
+    "name" : "P10001",
+    "serialNumber" : "P10001",
+    "eNode" : {
+        "thingTypeFieldId" : NumberLong(5),
+        "time" : ISODate("2016-03-24T16:09:15.346Z"),
+        "value" : "x3ed9371"
+    },
+    "status" : {
+        "thingTypeFieldId" : NumberLong(12),
+        "time" : ISODate("2016-03-24T16:19:03.728Z"),
+        "value" : "closed"
+    },
+    "path" : null
+});
+
+/* 10 */
+db.path_things.insert({
+    "_id" : NumberLong(10),
+    "serialNumber" : "RFID1004",
+    "path" : "9"
+});
+
+/* 11 */
+db.path_things.insert({
+    "_id" : NumberLong(11),
+    "serialNumber" : "CARTOON1001",
+    "path" : "9"
+});
+
+/* 12 */
+db.path_things.insert({
+    "_id" : NumberLong(12),
+    "serialNumber" : "RFID1005",
+    "path" : "9,11"
+});
+
+/* 13 */
+db.path_things.insert({
+    "_id" : NumberLong(13),
+    "serialNumber" : "BOX1001",
+    "path" : "9,11"
+});
+
+/* 14 */
+db.path_things.insert({
+    "_id" : NumberLong(14),
+    "serialNumber" : "RFID1006",
+    "path" : "9,11,13"
+});
+
+/* 15 */
+db.path_things.insert({
+    "_id" : NumberLong(15),
+    "serialNumber" : "ITEM1001",
+    "path" : "9,11,13"
+});
+
+/* 16 */
+db.path_things.insert({
+    "_id" : NumberLong(16),
+    "serialNumber" : "RFID1007",
+    "path" : "9,11,13,15"
+});
+
