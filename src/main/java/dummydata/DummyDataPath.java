df293b72ff04c5155c7397b50a881b0b6e80c0f8
Ruth Chirinos <ruth.chirinos@gmail.com>
2016-04-01 04:57:10 -0400

    MaterialixedPath rename of the class, standard java

--- src/main/java/dummydata/DummyDataPath.java
+++ src/main/java/dummydata/DummyDataPath.java
@@ -1,18 +1,19 @@
 package dummydata;
 
+import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
+import com.mongodb.WriteResult;
 import dao.MongoDAOUtil;
+import org.bson.types.ObjectId;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by rsejas on 3/31/16.
  */
 public class DummyDataPath {
     private static int MAX_THINGS = 20;
+    private static int MAX_SNAPSHOT_PER_THING = 2;
     private static int MAX_LEVELS = 6;
     private static String COLLECTION_NAME = "path_things";
     private static String[] THING_TYPE_CODES = {
@@ -73,8 +74,35 @@ public class DummyDataPath {
                 thingObject.put("pathThingType", (k == 0)?null:pathThingType);
                 thingObject.put("path", (k == 0)?null:path);
                 MongoDAOUtil.getInstance().getCollection(COLLECTION_NAME).save(thingObject);
+                //Create Snapshot
+                createSnapshot(thingObject);
             }
         }
+    }
+
+    public static void createSnapshot(BasicDBObject thingObject)
+    {
+        BasicDBList blinks = new BasicDBList();
+        for(int i=0;i<MAX_SNAPSHOT_PER_THING;i++)
+        {
+            Date date = new Date();
+            BasicDBObject snapshot = new BasicDBObject();
+            snapshot.put("value",thingObject);
+            snapshot.put("time",date);
+            MongoDAOUtil.getInstance().getCollection("thingSnapshotsPath").save(snapshot);
+            ObjectId objId = (ObjectId)snapshot.get( "_id" );
+
+            BasicDBObject blink = new BasicDBObject();
+            blink.put("time", date.getTime());
+            blink.put("blink_id", objId);
+            blinks.add(blink);
+        }
+
+
+        BasicDBObject snapshotId = new BasicDBObject();
+        snapshotId.put("_id",thingObject.get("_id"));
+        snapshotId.put("blinks",blinks);
+        MongoDAOUtil.getInstance().getCollection("thingSnapshotIdsPath").save(snapshotId);
     }
 
     private static List<Map<String, Object>> fillThingTypeList() {
@@ -90,8 +118,8 @@ public class DummyDataPath {
 
     private static Boolean initMongo() {
         try {
-//            MongoDAOUtil.setupMongodb("localhost", 27017, "riot_main", null , null, "admin", "control123!");
-            MongoDAOUtil.setupMongodb("10.100.1.140",27017, "riot_main", null , null, "vizix", "m0j1xInc!");
+            MongoDAOUtil.setupMongodb("localhost", 27017, "riot_main", null , null, "admin", "control123!");
+//            MongoDAOUtil.setupMongodb("10.100.1.140",27017, "riot_main", null , null, "vizix", "m0j1xInc!");
             return true;
         } catch (Exception e) {
             e.printStackTrace();
