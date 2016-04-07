928169ed7b7118b6052e99b166dda56220fe0cc3
rsejas <raul.sejas@mojix.com>
2016-04-07 17:59:32 -0400

    Modifying DummyDataTree.

--- src/main/java/dummydata/DummyDataTree.java
+++ src/main/java/dummydata/DummyDataTree.java
@@ -14,8 +14,8 @@ import java.util.concurrent.Executors;
 public class DummyDataTree {
     // INITIAL_ID = 1: Collection will be deleted
     // INITIAL_ID > 1: Collection will continue from this id
-    private static int nThreads = 18;
-    private static int INITIAL_ID = 150541;
+    private static int nThreads = 15;
+    private static int INITIAL_ID = 150655;
     private static int MAX_THINGS = 1000000;
     private static int MAX_THINGS_BY_DOC = 20;
     private static int MAX_LEVELS = 6;
