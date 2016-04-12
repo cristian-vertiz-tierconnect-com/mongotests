02db012904495d14ac3d4c40d0806be74a580808
rsejas <raul.sejas@mojix.com>
2016-04-12 10:55:39 -0400

    Modifying DummyDataTree. configuring to 20000 things

--- src/main/java/dummydata/DummyDataTree.java
+++ src/main/java/dummydata/DummyDataTree.java
@@ -15,9 +15,9 @@ import java.util.concurrent.Executors;
 public class DummyDataTree {
     // INITIAL_ID = 1: Collection will be deleted
     // INITIAL_ID > 1: Collection will continue from this id
-    private static int nThreads = 20;
+    private static int nThreads = 30;
     private static int INITIAL_ID = 1;
-    private static long MAX_THINGS = 1000000L;
+    private static long MAX_THINGS = 20000L;
     private static int MAX_THINGS_BY_DOC = 20;
     private static int MAX_LEVELS = 6;
     private static int MAX_BLINKS_PER_THING = 500;
